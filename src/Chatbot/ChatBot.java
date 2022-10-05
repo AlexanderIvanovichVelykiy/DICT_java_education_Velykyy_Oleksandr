dcb960da5ebcd7cbfe779525fdda93ca6591b848
AlexanderIvanovichVelykiy <o.i.velykyy@student.khai.edu>
2022-10-05 12:11:15 +0300

    second commit

--- src/Chatbot/ChatBot.java
+++ src/Chatbot/ChatBot.java
@@ -1,9 +1,13 @@
 package Chatbot;
+import java.util.Scanner;
 
 public class ChatBot {
     static void first(){
         System.out.println("Hello! My name is Mister Bomabass Dombass.");
         System.out.println("I was created in 2022.");
+        Scanner var = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        System.out.println("What a great name you have, "+  var.nextLine());
     }
     public static void main(String[] args) {
         first();
