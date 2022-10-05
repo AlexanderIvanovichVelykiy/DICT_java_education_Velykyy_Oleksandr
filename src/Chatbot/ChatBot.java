db4e68baf46cf3b4849b624f9d166f9f694f5ea5
AlexanderIvanovichVelykiy <o.i.velykyy@student.khai.edu>
2022-10-05 12:02:08 +0300

    first commit

---
+++ src/Chatbot/ChatBot.java
@@ -0,0 +1,11 @@
+package Chatbot;
+
+public class ChatBot {
+    static void first(){
+        System.out.println("Hello! My name is Mister Bomabass Dombass.");
+        System.out.println("I was created in 2022.");
+    }
+    public static void main(String[] args) {
+        first();
+    }
+}
