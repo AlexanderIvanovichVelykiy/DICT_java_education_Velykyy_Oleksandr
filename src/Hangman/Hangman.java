5abb9a56fe47c1fa6b37545852ef9024d7230ecd
AlexanderIvanovichVelykiy <o.i.velykyy@student.khai.edu>
2022-10-12 16:24:47 +0300

    first commit

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,13 @@
+package Hangman;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Hangman {
+    static void anonce(){
+        System.out.println("Hangman");
+        System.out.println("The game will available soon");
+    }
+    public static void main(String[] args) {
+        anonce();
+    }
+}
