671438fe751ad235626cdedec530d8755e31b851
AlexanderIvanovichVelykiy <o.i.velykyy@student.khai.edu>
2022-10-12 17:01:41 +0300

    third commit

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,12 +1,22 @@
 package Hangman;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Hangman {
-    static void anonce(){
-        Scanner answer = new Scanner(System.in);
-        String enter = answer.nextLine();
-        if (Objects.equals(enter, "Java")){
+    public static void game() {
+        ArrayList<String> var = new ArrayList<>();
+        var.add("java");
+        var.add("python");
+        var.add("kotlin");
+        var.add("C#");
+        Random random = new Random();
+        int randomIndex = random.nextInt(var.size());
+        String answer = var.get(randomIndex);
+        Scanner user = new Scanner(System.in);
+        String enter = user.nextLine();
+        if(Objects.equals(enter, answer)){
             System.out.println("You survive!");
         }
         else {
@@ -14,6 +24,6 @@ public class Hangman {
         }
     }
     public static void main(String[] args) {
-        anonce();
+        game();
     }
 }
