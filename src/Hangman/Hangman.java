f25ba8cfc9b2fd08cde25437afde4dfc113ac8c0
AlexanderIvanovichVelykiy <o.i.velykyy@student.khai.edu>
2022-10-15 14:58:40 +0300

    fifth commit

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,32 +1,90 @@
 package Hangman;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class Hangman {
-    public static void game() {
-        ArrayList<String> var = new ArrayList<>();
-        var.add("java");
-        var.add("python");
-        var.add("kotlin");
-        var.add("c++");
-        Random random = new Random();
-        int randomIndex = random.nextInt(var.size());
-        String answer = var.get(randomIndex);
-        int wl = answer.length();
-        int a = wl - 2;
-        System.out.println(answer.substring(0, 2) + " and "+ a + " symbols more");
-        Scanner user = new Scanner(System.in);
-        String enter = user.nextLine();
-        if(Objects.equals(enter, answer)){
-            System.out.println("You survive!");
-        }
-        else {
-            System.out.println("You lost!");
-        }
+    private String secretWord;
+    private char[] disguisedWord;
+    private int guessCount, chanceLeft;
+
+    public Hangman() {
+        guessCount = 0;
+        chanceLeft = 8;
+        secretWord = getWord();
+        disguisedWord = secretWord.toCharArray();
+        for (int i = 0; i < secretWord.length(); i++)
+            if (secretWord.charAt(i) == ' ') disguisedWord[i] = ' ';
+            else disguisedWord[i] = '-';
     }
+
+    public void makeGuess(char c) {
+        char a, b;
+        a = b = c;
+        a -= 32;
+        b += 32;
+        String x = new String(disguisedWord);
+
+        for (int i = 0; i < secretWord.length(); i++)
+            if (c == secretWord.charAt(i) || b == secretWord.charAt(i) || a == secretWord.charAt(i))
+                disguisedWord[i] = secretWord.charAt(i);
+
+        String y = new String(disguisedWord);    //After changing
+        if (x.equals(y)) chanceLeft--;    //If before is similar to after
+    }
+
+    public String getSecretWord() {
+        return secretWord;
+    }
+
+    public int getGuessCount() {
+        return ++guessCount;
+    }
+
+    public int getChanceLeft() {
+        return chanceLeft;
+    }
+
+    public char[] getDisguisedWord() {
+        return disguisedWord;
+    }
+
+    public boolean isFound() {
+        for (int i = 0; i < disguisedWord.length; i++)
+            if (disguisedWord[i] == '-') return false;
+        return true;
+    }
+
+    public String getWord() {
+        Random rand = new Random();
+        String[] wordList = {"Python", "Java", "JavaScript", "Kotlin"};
+        int r = rand.nextInt(wordList.length);
+        return wordList[r];
+    }
+
+
     public static void main(String[] args) {
-        game();
+
+        Hangman a1 = new Hangman();
+        System.out.println("""
+                Welcome to a game of Hangman!
+                The game will available soon!
+                """ + '\n');
+        Scanner key = new Scanner(System.in);
+
+        while (!a1.isFound()) {
+            System.out.println(a1.getDisguisedWord());
+            System.out.print("Please enter a character" + '\n');
+            a1.makeGuess(key.next().charAt(0));
+            System.out.println(a1.getDisguisedWord());
+            System.out.println("Guess count: " + a1.getGuessCount());
+            System.out.println("Chances left: " + a1.getChanceLeft());
+            if (a1.getChanceLeft() == 0) {
+                System.out.println("You lost!");
+                System.out.println("The secret word was: " + a1.getSecretWord());
+                break;
+            }
+            if (a1.isFound()) System.out.println("You survive!");
+        }
+        key.close();
     }
 }
