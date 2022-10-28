c9701d466dce921bbc9ddf0cd6cedc68c7ef6143
AlexanderIvanovichVelykiy <o.i.velykyy@student.khai.edu>
2022-10-28 13:42:04 +0300

    5-6 commit

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -22,59 +22,134 @@ public class CoffeeMachine {
     }
 
     public void espresso(){
-      Scanner espresso = new Scanner(System.in);
-      System.out.println("How many cups you want?");
-      int cup = espresso.nextInt();
-        water = water - (250*cup);
-        beans = beans - (16*cup);
-        cups = cups - cup;
-        money = (money + 4)*cup;
-        System.out.println("Milk: "+ milk);
-        System.out.println("Water: "+ water);
-        System.out.println("Beans: "+ beans);
-        System.out.println("Cups: "+ cups);
-        System.out.println("Money: "+ money);
-
+      while (true) {
+          Scanner espresso = new Scanner(System.in);
+          System.out.println("You want latte? (press 'back' if not or press any button if you want)");
+          String yon = espresso.nextLine();
+          if (yon.equals("back")) {
+             break;
+          }
+          System.out.println("How many cups you want?");
+          int cup = espresso.nextInt();
+          water = water - (250 * cup);
+          if(water < 0){
+              System.out.println("Enough water");
+              water = water + (250 * cup);
+              break;
+          }
+          beans = beans - (16 * cup);
+          if(beans < 0){
+              System.out.println("Enough beans");
+              beans = beans + (16 * cup);
+              break;
+          }
+          cups = cups - cup;
+          if(cups < 0){
+              System.out.println("Enough cups");
+              cups = cups + cup;
+              break;
+          }
+          money = (money + 4) * cup;
+          System.out.println("Milk: " + milk);
+          System.out.println("Water: " + water);
+          System.out.println("Beans: " + beans);
+          System.out.println("Cups: " + cups);
+          System.out.println("Money: " + money);
+          break;
+      }
     }
     public  void capuccino(){
-        Scanner espresso = new Scanner(System.in);
+        while (true){
+        Scanner capuccino = new Scanner(System.in);
+        System.out.println("You want cappuccino? (press 'back' if not or press any button if you want)");
+        String yon = capuccino.nextLine();
+        if(yon.equals("back")){
+            break;
+        }
         System.out.println("How many cups you want?");
-        int cup = espresso.nextInt();
+        int cup = capuccino.nextInt();
         milk = milk - (100*cup);
+            if(milk < 0){
+                System.out.println("Enough milk");
+                milk = milk + (100*cup);
+                break;
+            }
         water = water - (200*cup);
+            if(water < 0){
+                System.out.println("Enough water");
+                water = water + (200*cup);
+                break;
+            }
         beans = beans - (12*cup);
+            if(beans < 0){
+                System.out.println("Enough beans");
+                beans = beans + (12*cup);
+                break;
+            }
         cups = cups - cup;
+            if(cups < 0){
+                System.out.println("Enough cups");
+                cups = cups + cup;
+                break;
+            }
         money = (money + 6)*cup;
         System.out.println("Milk: "+ milk);
         System.out.println("Water: "+ water);
         System.out.println("Beans: "+ beans);
         System.out.println("Cups: "+ cups);
         System.out.println("Money: "+ money);
+        break;
+    }
     }
     public  void latte(){
-        Scanner espresso = new Scanner(System.in);
+        while (true){
+        Scanner latte = new Scanner(System.in);
+        System.out.println("You want latte? (press 'back' if not or press any button if you want)");
+        String yon = latte.nextLine();
+        if (yon.equals("back")){
+            break;
+        }
         System.out.println("How many cups you want?");
-        int cup = espresso.nextInt();
+        int cup = latte.nextInt();
         milk = milk - (75*cup);
+            if(milk < 0){
+                System.out.println("Enough milk");
+                milk = milk + (75*cup);
+                break;
+            }
         water = water - (360*cup);
+            if(water < 0){
+                System.out.println("Enough water");
+                water = water + (360*cup);
+                break;
+            }
         beans = beans - (20*cup);
+            if(beans < 0){
+                System.out.println("Enough beans");
+                beans = beans + (20*cup);
+                break;
+            }
         cups = cups - cup;
+            if(cups < 0){
+                System.out.println("Enough cups");
+                cups = cups + cup;
+                break;
+            }
         money = (money + 7)*cup;
         System.out.println("Milk: "+ milk);
         System.out.println("Water: "+ water);
         System.out.println("Beans: "+ beans);
         System.out.println("Cups: "+ cups);
         System.out.println("Money: "+ money);
+        break;
     }
-
-
-
+    }
     public static void main(String[] args) {
         CoffeeMachine cof = new CoffeeMachine();
         System.out.println("The coffee machine has:\n"+ cof.milk +" milk \n"+ cof.water+ " water\n"+ cof.beans+" beans\n"+ cof.cups+ " cups\n"+ cof.money+" money");
         while (true){
         Scanner action = new Scanner(System.in);
-        System.out.println("Write action (1.buy, 2.fill, 3.take, 4.exit):");
+        System.out.println("Write action (1.buy, 2.fill, 3.take, 4.remaining , 5.exit):");
         int act = action.nextInt();
             switch (act) {
                 case 1 -> cof.buy();
@@ -85,20 +160,32 @@ public class CoffeeMachine {
                     cof.milk = cof.milk + add_milk;
                     System.out.println("How many water you want?");
                     int add_water = add.nextInt();
-                    cof.milk = cof.milk + add_water;
+                    cof.water = cof.water + add_water;
                     System.out.println("How many beans you want?");
                     int add_beans = add.nextInt();
-                    cof.milk = cof.milk + add_beans;
+                    cof.beans = cof.beans + add_beans;
                     System.out.println("How many cups you want?");
                     int add_cups = add.nextInt();
-                    cof.milk = cof.milk + add_cups;
+                    cof.cups = cof.cups + add_cups;
                 }
                 case 3 -> {
                     System.out.println(cof.money);
                     cof.money = 0;
                 }
+                case 4 -> {
+                    System.out.println("milk:");
+                    System.out.println(cof.milk);
+                    System.out.println("water:");
+                    System.out.println(cof.water);
+                    System.out.println("beans:");
+                    System.out.println(cof.beans);
+                    System.out.println("cups:");
+                    System.out.println(cof.cups);
+                    System.out.println("money:");
+                    System.out.println(cof.money);
+                }
             }
-        if(act == 4){
+        if(act == 5){
             break;
         }
         }
